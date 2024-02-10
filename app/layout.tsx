938ea00e0755088aedb59afc0928352fdfe286cd
Alex Grintsvayg <git@grin.io>
2024-02-10 12:14:07 -0500

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/img/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/img/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/img/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/img/site.webmanifest" />
+        <link rel="shortcut icon" href="/img/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="msapplication-config" content="/img/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className={inter.className}>
         {children}
         <Analytics />
