67f61df23cd88d74c0c8ee91d64d8fe5d3a71326
Alex Grintsvayg <git@grin.io>
2024-02-10 11:19:49 -0500

    first

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,54 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import { groups } from "@/data/groups";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <h1>Open Warpcast Groups</h1>
+      <div className={styles.groups}>
+        {groups.map((group) => (
+          <div className={styles.group} key={group.name}>
+            <div className={styles.groupTop}>
+              <div className={styles.nameAndPic}>
+                <img
+                  src={group.image}
+                  alt={group.name}
+                  width={40}
+                  height={40}
+                />
+                <div className={styles.name}>{group.name}</div>
+              </div>
+              <div className={styles.join}>
+                <Link className={styles.link} href={group.inviteLink}>
+                  Join
+                </Link>{" "}
+              </div>
+            </div>
+            <div className={styles.description}>{group.description}</div>
+            {group.requiredNFT && (
+              <div className={styles.requiredNFT}>
+                Requires{" "}
+                <Link
+                  className={styles.link}
+                  href={group.requiredNFT}
+                  target={"_blank"}
+                >
+                  {group.requiredNFT.substring(
+                    group.requiredNFT.lastIndexOf("/") + 1,
+                  )}
+                </Link>
+              </div>
+            )}
+          </div>
+        ))}
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        Made by{" "}
+        <Link className={styles.link} href={"https://warpcast.com/grin"}>
+          Grin
+        </Link>
+        .
       </div>
     </main>
   );
---
+++ data/groups.ts
@@ -0,0 +1,38 @@
+type Group = {
+  name: string;
+  description: string;
+  image: string;
+  inviteLink: string;
+  requiredNFT: string | null;
+};
+
+export const groups: Group[] = [
+  {
+    name: "FarCon '24 Summit",
+    description: "FarCon 2024 Summit Day (thursday) ticketholders",
+    image: "https://raw.seadn.io/files/39345793c7da0d41bba2f86bf57fb46f.webp",
+    inviteLink: "https://warpcast.com/~/group/2wSFJwP2POel0vY6Ex4dCw",
+    requiredNFT: "https://opensea.io/collection/farcon-summit-pass",
+  },
+  {
+    name: "FarCon OGs",
+    description: "Attendees of the first FarCon in Boston",
+    image: "https://raw.seadn.io/files/b42b3c7020db7abd0ece388f9b6c55bb.jpg",
+    inviteLink: "https://warpcast.com/~/group/PgRIHNiyrBUPNom1dqWOag",
+    requiredNFT: "https://opensea.io/collection/farcon-2023-tickets",
+  },
+  {
+    name: "Cabin Citizens",
+    description: "Citizens of cabin.city",
+    image: "https://raw.seadn.io/files/13007d5982c17aa1b7256337221dec17.png",
+    inviteLink: "https://warpcast.com/~/group/3SQdOvlnuqQzny0PAhelQA",
+    requiredNFT: "https://opensea.io/collection/cabin-citizenship",
+  },
+  {
+    name: "the haberdashery",
+    description: "degendao gc",
+    image: "https://i.imgur.com/jDU5tsS.jpg",
+    inviteLink: "https://warpcast.com/purp",
+    requiredNFT: "https://opensea.io/collection/degen-haberdashers",
+  },
+];
