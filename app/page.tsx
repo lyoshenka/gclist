61a58201f8b0182dcbc2629c6ac9147a0209feb3
Alex Grintsvayg <git@grin.io>
2024-02-23 11:00:19 -0500

    sort groups

--- app/page.tsx
+++ app/page.tsx
@@ -7,42 +7,46 @@ export default function Home() {
     <main className={styles.main}>
       <h1>Open Warpcast Groups</h1>
       <div className={styles.groups}>
-        {groups.map((group) => (
-          <div className={styles.group} key={group.name}>
-            <div className={styles.groupTop}>
-              <div className={styles.nameAndPic}>
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                  src={group.image}
-                  alt={group.name}
-                  width={40}
-                  height={40}
-                />
-                <div className={styles.name}>{group.name}</div>
+        {groups
+          .sort((a, b) =>
+            a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+          )
+          .map((group) => (
+            <div className={styles.group} key={group.name}>
+              <div className={styles.groupTop}>
+                <div className={styles.nameAndPic}>
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img
+                    src={group.image}
+                    alt={group.name}
+                    width={40}
+                    height={40}
+                  />
+                  <div className={styles.name}>{group.name}</div>
+                </div>
+                <div className={styles.join}>
+                  <Link className={styles.link} href={group.inviteLink}>
+                    Join
+                  </Link>{" "}
+                </div>
               </div>
-              <div className={styles.join}>
-                <Link className={styles.link} href={group.inviteLink}>
-                  Join
-                </Link>{" "}
-              </div>
+              <div className={styles.description}>{group.description}</div>
+              {group.requiredNFT && (
+                <div className={styles.requiredNFT}>
+                  Requires{" "}
+                  <Link
+                    className={styles.link}
+                    href={group.requiredNFT}
+                    target={"_blank"}
+                  >
+                    {group.requiredNFT.substring(
+                      group.requiredNFT.lastIndexOf("/") + 1
+                    )}
+                  </Link>
+                </div>
+              )}
             </div>
-            <div className={styles.description}>{group.description}</div>
-            {group.requiredNFT && (
-              <div className={styles.requiredNFT}>
-                Requires{" "}
-                <Link
-                  className={styles.link}
-                  href={group.requiredNFT}
-                  target={"_blank"}
-                >
-                  {group.requiredNFT.substring(
-                    group.requiredNFT.lastIndexOf("/") + 1,
-                  )}
-                </Link>
-              </div>
-            )}
-          </div>
-        ))}
+          ))}
       </div>
       <div className={styles.add}>
         <Link
